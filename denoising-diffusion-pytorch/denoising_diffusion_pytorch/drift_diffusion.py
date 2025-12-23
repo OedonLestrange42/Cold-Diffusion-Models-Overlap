0f2de28e5fe70b129e09945ff5daada3c06bee06
oedon42 <zxbsmk@gmail.com>
2025-12-23 14:05:44 +0800

    bug fix

--- denoising-diffusion-pytorch/denoising_diffusion_pytorch/drift_diffusion.py
+++ denoising-diffusion-pytorch/denoising_diffusion_pytorch/drift_diffusion.py
@@ -129,7 +129,7 @@ def linear_beta_schedule(timesteps):
     scale = 1000 / timesteps
     beta_start = scale * 0.0001
     beta_end = scale * 0.02
-    return torch.linspace(beta_start, beta_end, timesteps, dtype = torch.float64)
+    return torch.linspace(beta_start, beta_end, timesteps, dtype = torch.float32)
 
 class DriftDiffusion(GaussianDiffusion):
     def __init__(
